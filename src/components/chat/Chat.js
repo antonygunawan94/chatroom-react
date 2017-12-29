af9f966635f3a547046622443068b4ddfc5af713
Antony Gunawan <antonygunawan94@gmail.com>
2017-12-29 11:48:33 +0700

    add connection to ws

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -7,12 +7,38 @@ class Chat extends Component {
     constructor() {
         super();
         this.state = {
-            chats: []
+            chats: [],
+            socket: null,
+            socketStatus : "Loading..."       
         };
     }
 
-    connectToWs(){
+    componentDidMount(){
+        this.connectToWs();        
+    }
 
+    connectToWs(){
+        let socket = new WebSocket("ws://localhost:8080/ws?channel=asd")
+        socket.addEventListener('open', () => {
+            this.setState({socketStatus : "Connected..." })
+        })
+        socket.addEventListener('error', (error) => {
+            this.setState({ socketStatus: "Error..." })            
+        })
+        socket.addEventListener('message', (event) => {
+            this.receiveMessage(event.data)
+        })
+
+        this.setState({ socket: socket })
+    }
+
+    sendMessage(chat){
+        this.state.socket.send(JSON.stringify(chat))
+    }
+
+    receiveMessage(data){
+        let chat = JSON.parse(data)
+        this.setState({ chats: this.state.chats.concat(chat) })
     }
 
     render() {
@@ -21,11 +47,11 @@ class Chat extends Component {
                 <div className="tile is-parent is-vertical is-marginless box">
                     <h1 className="title has-text-centered is-marginless">Channel</h1>
                     <hr style={{ margin: "5px 0" }}/>
-                    <h3 className="subtitle has-text-centered is-5">Socket Connection Status</h3>
+                    <h3 className="subtitle has-text-centered is-5">{ this.state.socketStatus }</h3>
                 </div>
                 <ChatList chats={this.state.chats}/>
                 <ChatAction onChatSubmit={ (chat) => {
-                    this.setState({ chats: this.state.chats.concat(chat) })    
+                        this.sendMessage(chat)
                 } } />
             </div>
         );
